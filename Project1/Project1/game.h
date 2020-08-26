bce37760f4192db5046e3de9f7c88bc3aafd555a
King-juejue <2398050701@qq.com>
2020-08-26 16:28:40 +0800

    三子棋

---
+++ Project1/Project1/game.h
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#define ROW 3
+#define COL 3
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+//ÉùÃ÷
+void InitBoard(char board[ROW][COL], int row, int col);
+void DispalyBoard(char board[ROW][COL], int row, int col);
+void PlayerMove(char board[ROW][COL],int row,int col);
+void ComputerMove(char board[ROW][COL],int row,int col);
+
+
+char IsWin(char board[ROW][COL],int row,int col);
+
+
