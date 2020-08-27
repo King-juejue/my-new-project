24c27f529298276fe5ba2ab5d76b97c53aed6412
King-juejue <2398050701@qq.com>
2020-08-27 22:43:17 +0800

    my_strcpy

---
+++ test1/my_strcpy.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+void my_strcpy(char* dest, char* src)
+{
+	while (*src!='\0')
+	{
+		*dest = *src;
+		src++;
+		dest++;
+	}
+	*dest = *src;
+}
+
+
+int main()
+{
+	char arr1[] = "##########";
+	char arr2[] = "bit";
+	my_strcpy(arr1, arr2);
+	printf("%s\n", arr1);
+	printf("%s\n", arr2);
+
+
+	return 0;
+}
